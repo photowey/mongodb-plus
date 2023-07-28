8017bdb186d2c3b1857b4161198164a1c7737d7c
photowey <photowey@gmail.com>
2023-07-28 22:49:34 +0800

    feat: Add: utils ut.

---
+++ mongodb-plus-core/src/test/java/com/photowey/mongodb/plus/core/util/ObjectUtilsTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright © 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.mongodb.plus.core.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+
+/**
+ * {@code ObjectUtilsTest}
+ *
+ * @author photowey
+ * @date 2023/07/28
+ * @since 1.0.0
+ */
+class ObjectUtilsTest {
+
+    @Test
+    void testIsNull() {
+        Person person = null;
+
+        boolean isNullTrue = ObjectUtils.isNull(person);
+        Assertions.assertTrue(isNullTrue);
+
+        person = new Person();
+
+        boolean isNullFalse = ObjectUtils.isNull(person);
+        Assertions.assertFalse(isNullFalse);
+    }
+
+    @Test
+    void testIsNotNull() {
+        Person person = null;
+
+        boolean isNotNullFalse = ObjectUtils.isNotNull(person);
+        Assertions.assertFalse(isNotNullFalse);
+
+        person = new Person();
+
+        boolean isNotNotNullTure = ObjectUtils.isNotNull(person);
+        Assertions.assertTrue(isNotNotNullTure);
+    }
+
+    public static class Person implements Serializable {
+
+        private static final long serialVersionUID = -1316830162729572943L;
+
+        private Long id;
+        private String name;
+        private Integer age;
+        private String helloWorld;
+        private boolean successful;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public String getHelloWorld() {
+            return helloWorld;
+        }
+
+        public void setHelloWorld(String helloWorld) {
+            this.helloWorld = helloWorld;
+        }
+
+        public boolean isSuccessful() {
+            return successful;
+        }
+
+        public void setSuccessful(boolean successful) {
+            this.successful = successful;
+        }
+    }
+}
